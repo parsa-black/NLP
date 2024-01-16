ec2f6c163435ac95df090d9600c50454ce589021
parsa <parsagorgzan1225@gmail.com>
2024-01-17 02:35:08 +0330

    Laplace Smoothing

--- Classification.py
+++ Classification.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import glob
 import string
 
 
@@ -17,16 +18,41 @@ def create_word_dictionary(text_content):
     return word_dictionary
 
 
-file_path = "DataSets/Classification-Train And Test/Comp.graphics/data554.txt"
+def get_total_word_dictionary(folder_path):
+    total_word_dictionary = defaultdict(int)
+    file_paths = glob.glob(f"{folder_path}/*.txt")
+    for file_path in file_paths:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            file_content = file.read()
+            file_dictionary = create_word_dictionary(file_content)
+            for word, count in file_dictionary.items():
+                total_word_dictionary[word] += count
 
-# Read File
-with open(file_path, 'r', encoding='utf-8') as file:
-    test_set_text = file.read()
+    return total_word_dictionary
 
-# Create Dictionary from TestSet
-test_set_dictionary = create_word_dictionary(test_set_text)
 
-# نمایش دیکشنری
+def get_total_word_count(folder_path):
+    total_word_count = 0
+    file_paths = glob.glob(f"{folder_path}/*.txt")
+    for file_path in file_paths:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            file_content = file.read()
+            words = preprocess_text(file_content)
+            total_word_count += len(words)
+
+    return total_word_count
+
+
+Comp_folder_path = "DataSets/Classification-Train And Test/Comp.graphics"
+Comp_Dict = get_total_word_dictionary(Comp_folder_path)
+Comp_Count = get_total_word_count(Comp_folder_path)
+Comp_V = len(Comp_Dict)
+
+# Laplace Smoothing
+for word in Comp_Dict:
+    Comp_Dict[word] += 1
+    Comp_Dict[word] /= (Comp_Count + Comp_V)
+
 print("Test set dictionary:")
-for word, count in test_set_dictionary.items():
+for word, count in Comp_Dict.items():
     print(f"{word}: {count}")
